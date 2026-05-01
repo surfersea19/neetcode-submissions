fdece42aaa31506ec4fbb79a95cc04e1bc60facc
surfersea19 <surfersea4@gmail.com>
2026-05-01 18:16:25 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+        return 0;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if (s==t)
+        return 1;
+        return 0;
+        
+    }
+};
