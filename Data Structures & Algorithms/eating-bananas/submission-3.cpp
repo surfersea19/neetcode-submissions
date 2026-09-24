47e0e421e78af5511f99375af7209827598f43d1
surfersea19 <surfersea4@gmail.com>
2026-09-24 22:59:12 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1;
+        int r=*max_element(piles.begin(),piles.end());
+        int res;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            long long k=0;
+            for(int nums:piles)
+            {
+                if(nums%mid==0)
+                {
+                    k+=(nums/mid);
+                }
+                else
+                {
+                    k+=((nums/mid)+1);
+                }
+            }
+            if(k<=h)
+            {
+                res=mid;
+                r=mid-1;
+            }
+            if(k>h)
+            {
+                l=mid+1;
+            }
+        }
+        return res;
+
+        
+    }
+};
